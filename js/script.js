3fa59b4aea20e1944852057adbeffa058f8a4b4e
Adeke Msonter Samuel <128712012+adekemsontersamuel@users.noreply.github.com>
2025-01-19 13:19:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName('close')[0];
+
+    // Show the modal when the page loads
+    modal.style.display = 'block';
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+
+    // Add event listener to the modal button (optional)
+    var modalButton = document.getElementById('modalButton');
+    modalButton.addEventListener('click', function() {
+        window.location.href = '/academy.html';
+        //alert('Button inside modal clicked!');
+        // You can add more actions here
+    });
+});
